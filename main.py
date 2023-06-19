fc0d43aa7ed261ba37f23a72aed6dcad093fc54c
GraziMarinoni <grazi.marinoni@gmail.com>
2023-06-19 10:26:48 +0100

    Phase 1

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
+
+# webpage to be scraped
 page = requests.get("http://books.toscrape.com/catalogue/under-the-tuscan-sun_504")
 
+# relevant data to be scraped
 headings = ["product_page_url", "universal_product_code (upc)", "book_title", "price_incl_tax", "price_excl_tax", "qnt_available", "product_description", "category", "review_rating", "image_url"]
+# pulling the HTML content of the current page
 soup = BeautifulSoup(page.content, "html.parser")
 # print(soup.prettify())
 
+
 book_title = soup.find("li", class_="active").get_text()
 UPC = soup.find("th", string="UPC").next_sibling.get_text()
-price_excl = soup.find("th", string="Price (excl. tax)").next_sibling.get_text()
-price_incl = soup.find("th", string="Price (incl. tax)").next_sibling.get_text()
+# UPC = short for * universal product code *
+price_excl_tax = soup.find("th", string="Price (excl. tax)").next_sibling.get_text()
+# price excluding taxes
+price_incl_tax = soup.find("th", string="Price (incl. tax)").next_sibling.get_text()
+# price including taxes
 qnt_available = soup.find("p", class_="instock availability").get_text().replace("In stock (", "").replace("available)", "")
-img_url = soup.find("img", attrs={"alt": book_title})["src"].replace("../../media", "http://books.toscrape.com/media")
+# quantity available
+image_url = soup.find("img", attrs={"alt": book_title})["src"].replace("../../media", "http://books.toscrape.com/media")
 product_description = soup.find("div", attrs={"id": "product_description", "class": "sub-header"}).find_next_sibling("p").get_text()
 category = soup.find("ul", class_="breadcrumb").find_all("a", limit=3)[-1].get_text()
 review_rating = soup.find("p", class_="instock availability").find_next_sibling("p")["class"][-1]
+# star rating review method
+product_page_url = soup.findAll(string=lambda string: isinstance(string, Comment))[9].replace('\n', '').replace(" ", "").replace('<aid="write_review"href="/', '').replace('/reviews/add/#addreview"class="btnbtn-successbtn-sm">Writeareview</a>', '').replace('catalogue/', 'https://books.toscrape.com/catalogue/')
 
-product_page_url = page.url
-print(product_page_url)
+header_values = [product_page_url, UPC, book_title, price_incl_tax, price_excl_tax, qnt_available, product_description, category, review_rating, image_url]
 
-
-# soup.find("div", class_="col-sm-6 product_main")
+print(header_values)
