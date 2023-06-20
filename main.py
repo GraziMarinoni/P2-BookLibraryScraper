7f50490e5f59ab916f7b39be6de6a7e9b0700a6c
GraziMarinoni <grazi.marinoni@gmail.com>
2023-06-20 14:34:26 +0100

    Adjustments in phase 3

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 import csv
 
 # relevant data to be scraped
-headings = ["product_page_url", "universal_product_code (upc)", "book_title", "price_incl_tax", "price_excl_tax", "qnt_available", "product_description", "category", "review_rating", "image_url"]
-bookURL = "http://books.toscrape.com/catalogue/under-the-tuscan-sun_504"
+headings = ["category", "book_title", "product_page_url", "universal_product_code (upc)", "price_incl_tax", "price_excl_tax", "qnt_available", "product_description", "review_rating", "image_url"]
+# bookURL = "http://books.toscrape.com/catalogue/under-the-tuscan-sun_504"
+
 
 with open('data.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     writer.writerow(headings)
+
 
 def details(url):
     # webpage to be scraped
@@ -18,7 +20,9 @@ def details(url):
 
     # print(soup.prettify())  >>>> This is a command
 
+    category = soup.find("ul", class_="breadcrumb").find_all("a", limit=3)[-1].get_text()
     book_title = soup.find("li", class_="active").get_text()
+    product_page_url = page.url
     UPC = soup.find("th", string="UPC").next_sibling.get_text()
     # UPC = short for * universal product code *
     price_excl_tax = soup.find("th", string="Price (excl. tax)").next_sibling.get_text()
@@ -27,14 +31,12 @@ def details(url):
     # price including taxes
     qnt_available = soup.find("p", class_="instock availability").get_text().replace("In stock (", "").replace("available)", "")
     # quantity available
-    image_url = soup.find("img", attrs={"alt": book_title})["src"].replace("../../media", "http://books.toscrape.com/media")
     product_description = soup.find("div", attrs={"id": "product_description", "class": "sub-header"}).find_next_sibling("p").get_text()
-    category = soup.find("ul", class_="breadcrumb").find_all("a", limit=3)[-1].get_text()
     review_rating = soup.find("p", class_="instock availability").find_next_sibling("p")["class"][-1]
     # star rating review method
-    product_page_url = page.url
+    image_url = soup.find("img", attrs={"alt": book_title})["src"].replace("../../media", "http://books.toscrape.com/media")
 
-    header_values = [product_page_url, UPC, book_title, price_incl_tax, price_excl_tax, qnt_available, product_description, category, review_rating, image_url]
+    header_values = [category, book_title, product_page_url, UPC, price_incl_tax, price_excl_tax, qnt_available, product_description, review_rating, image_url]
     # print(header_values)   >>>> This is a command
 
     with open('data.csv', 'a', newline='') as csvfile:
@@ -43,7 +45,7 @@ def details(url):
     return
 
 # Calling the function for the first book URL
-details(bookURL)
+# details(bookURL)
 # End of phase 1
 
 # Start of phase 2
@@ -70,8 +72,8 @@ def scan_page():
     return formatted_html
 
 
-def find_all_categories_links(html):
-    categories = html.find("div", class_="side_categories")
+def find_all_categories_links(formatted_html):
+    categories = formatted_html.find("div", class_="side_categories")
     links = categories.find_all("a")
     del links[0]
     return links
@@ -81,6 +83,7 @@ def find_all_category_books(category):
     for book in category:
         print(book)
 
+
 def phase_three():
     html = scan_page()
     categories = find_all_categories_links(html)
@@ -88,11 +91,13 @@ def phase_three():
         find_all_category_books(category)
 
 
-categories = find_all_categories_links()
+html = scan_page()
+categories = find_all_categories_links(html)
 for category in categories:
     book_address = category["href"]
     book_url = book_address.replace("../","http://books.toscrape.com/catalogue/category/")
-    print(book_address)
+    print(book_url)
+
 
 # for i in range(len(meta_list)):
 #     meta_url = meta_list[i].find("li").find("a")["href"].replace("../../../","http://books.toscrape.com/catalogue/")
